51b19da28b4c5a847e7a28aff53dfbc109c390ec
Alexei Frolov <alexeifrolov3@gmail.com>
2016-10-26 01:11:50 -0400

    Add a few files

---
+++ arch/i386/vga.h
@@ -0,0 +1,53 @@
+/*
+ * arch/i386/vga.h
+ * Copyright (C) 2016 Alexei Frolov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef OS_ARCH_I386_VGA_H
+#define OS_ARCH_I386_VGA_H
+
+#include <stdint.h>
+
+enum vga_color {
+	VGA_COLOR_BLACK,
+	VGA_COLOR_BLUE,
+	VGA_COLOR_GREEN,
+	VGA_COLOR_CYAN,
+	VGA_COLOR_RED,
+	VGA_COLOR_MAGENTA,
+	VGA_COLOR_BROWN,
+	VGA_COLOR_LIGHT_GREY,
+	VGA_COLOR_DARK_GREY,
+	VGA_COLOR_LIGHT_BLUE,
+	VGA_COLOR_LIGHT_GREEN,
+	VGA_COLOR_LIGHT_CYAN,
+	VGA_COLOR_LIGHT_RED,
+	VGA_COLOR_LIGHT_MAGENTA,
+	VGA_COLOR_LIGHT_BROWN,
+	VGA_COLOR_WHITE
+};
+
+static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg)
+{
+	return fg | bg << 4;
+}
+
+static inline uint16_t vga_entry(uint8_t c, uint8_t color)
+{
+	return (uint16_t)uc | (uint16_t)color << 8;
+}
+
+#endif /* OS_ARCH_I386_VGA_H */
