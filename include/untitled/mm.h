e97d67e1401eb78aab8157fd62860611ee4051c8
Alexei Frolov <alexeifrolov3@gmail.com>
2016-11-21 22:37:06 -0500

    Some buddy definitions

--- include/untitled/mm.h
+++ include/untitled/mm.h
@@ -27,17 +27,38 @@
 #include <untitled/page.h>
 
 #define KERNEL_VIRTUAL_BASE	__ARCH_KERNEL_VIRT_BASE
+#define KERNEL_SIZE		0x00400000
 
+/*
+ * Page map starts at 16 MiB in physical memory, directly after the DMA zone.
+ */
 #define __PAGE_MAP_PHYS_BASE	0x01000000
 #define PAGE_MAP_BASE		(KERNEL_VIRTUAL_BASE + __PAGE_MAP_PHYS_BASE)
 
 extern uint64_t totalmem;
 
-void page_map_init(void);
-void detect_memory(multiboot_info_t *mbt);
+void buddy_init(struct multiboot_info *mbt);
 
-struct page *alloc_page(void);
+
+/*
+ * Page allocation order.
+ * The maximum amount of pages that can be allocated
+ * at a time is 2^(PAGE_MAX_ORDER - 1).
+ */
+#define PA_MAX_ORDER 10
+
+/* Page allocation flags */
+#define PA_STANDARD	0x0
+#define PA_ZONE_DMA	0x1
+
+struct page *alloc_pages(unsigned int flags, size_t ord);
 void free_page(void *base);
+
+static __always_inline struct page *alloc_page(unsigned int flags)
+{
+	return alloc_pages(flags, 0);
+}
+
 
 #define phys_addr(x) __pa((addr_t)x)
 
