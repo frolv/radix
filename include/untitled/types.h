328db2aecde1e54fa93b4ea5db1f72384b8513c7
Alexei Frolov <alexeifrolov3@gmail.com>
2016-12-06 22:22:47 -0500

    Add a couple that I missed

--- include/untitled/types.h
+++ include/untitled/types.h
@@ -24,8 +24,10 @@
 #define NULL ((void *)0UL)
 
 typedef unsigned long	size_t;
+typedef signed long	ssize_t;
+typedef signed long	off_t;
 
-typedef int		pid_t;
+typedef signed int	pid_t;
 typedef unsigned int	uid_t;
 typedef unsigned int	gid_t;
 typedef unsigned int	mode_t;
