a9e192480fedefc332d9720194a36091d5da7c3b
Alexei Frolov <alexeifrolov3@gmail.com>
2017-01-31 23:29:53 -0500

    Read CPU information and flags

--- arch/i386/cpu/cpu.c
+++ arch/i386/cpu/cpu.c
@@ -19,20 +19,31 @@
 #include <string.h>
 #include <untitled/cpu.h>
 
+static long cpuid_max;
+
+/* CPU vendor string */
 static char vendor_id[16];
+
+/* cpuid 0x1 information */
+static long cpu_info[4];
 
 void read_cpu_info(void)
 {
-	int eax;
-	int vendor[4];
+	long vendor[4];
 
 	if (!cpuid_supported()) {
 		vendor_id[0] = '\0';
 		return;
 	}
 
-	cpuid(0, eax, vendor[0], vendor[2], vendor[1]);
+	cpuid(0, cpuid_max, vendor[0], vendor[2], vendor[1]);
 	vendor[3] = 0;
-
 	memcpy(vendor_id, vendor, sizeof vendor_id);
+
+	cpuid(1, cpu_info[0], cpu_info[1], cpu_info[2], cpu_info[3]);
+}
+
+int cpu_has_apic(void)
+{
+	return cpu_info[3] & CPUID_APIC;
 }
