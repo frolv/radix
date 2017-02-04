05771c3077dce310e00f61c40c4c85c2acb6c07a
Alexei Frolov <alexeifrolov3@gmail.com>
2017-02-03 23:12:28 -0500

    Add required include

--- arch/i386/mm/pagetable.c
+++ arch/i386/mm/pagetable.c
@@ -16,6 +16,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <string.h>
 #include <untitled/kernel.h>
 #include <untitled/mm.h>
 #include <untitled/page.h>
