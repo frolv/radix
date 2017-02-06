49ff43343fbf02b5fc2d33cae91a611866dd73f2
Alexei Frolov <alexeifrolov3@gmail.com>
2017-02-05 20:00:13 -0500

    ACPI RSDP definitions

---
+++ include/acpi/rsdp.h
@@ -0,0 +1,42 @@
+/*
+ * include/acpi/rsdp.h
+ * Copyright (C) 2016-2017 Alexei Frolov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ACPI_RSDP_H
+#define ACPI_RSDP_H
+
+#include <untitled/types.h>
+
+/* ACPI Root System Description Pointer descriptor. */
+struct acpi_rsdp {
+	char signature[8];
+	uint8_t checksum;
+	char oem_id[6];
+	uint8_t revision;
+	uint32_t rsdt_addr;
+};
+
+/* Extended descriptor introduced in ACPI 2.0 (x86_64 support) */
+struct acpi_rsdp_2 {
+	struct acpi_rsdp rsdp;
+	uint32_t length;
+	uint64_t xsdt_addr;
+	uint8_t ext_checksum;
+	uint8_t reserved[3];
+};
+
+#endif /* ACPI_RSDP_H */
