4f2aaa2c4123e257ed67d626503546d917646adf
Alexei Frolov <alexeifrolov3@gmail.com>
2017-02-08 22:46:49 -0500

    Struct definitions for MADT data

---
+++ include/acpi/tables/madt.h
@@ -0,0 +1,196 @@
+/*
+ * include/acpi/tables/madt.h
+ * Copyright (C) 2016-2017 Alexei Frolov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ACPI_TABLES_MADT_H
+#define ACPI_TABLES_MADT_H
+
+#include <acpi/tables/sdt.h>
+
+enum {
+	ACPI_MADT_LOCAL_APIC,
+	ACPI_MADT_IO_APIC,
+	ACPI_MADT_INTERRUPT_OVERRIDE,
+	ACPI_MADT_NMI_SOURCE,
+	ACPI_MADT_LOCAL_APIC_NMI,
+	ACPI_MADT_LOCAL_APIC_OVERRIDE,
+	ACPI_MADT_IO_SAPIC,
+	ACPI_MADT_LOCAL_SAPIC,
+	ACPI_MADT_INTERRUPT_SOURCE,
+	ACPI_MADT_LOCAL_X2APIC,
+	ACPI_MADT_LOCAL_X2APIC_NMI,
+	ACPI_MADT_GENERIC_INTERRUPT,
+	ACPI_MADT_GENERIC_DISTRIBUTOR,
+	ACPI_MADT_GENERIC_MSI_FRAME,
+	ACPI_MADT_GENERIC_REDISTRIBUTOR,
+	ACPI_MADT_GENERIC_TRANSLATOR
+};
+
+struct acpi_madt {
+	struct acpi_sdt_header header;
+	uint32_t address;
+	uint32_t flags;
+};
+
+/*
+ * Corresponding subtables for above types.
+ * From Linux include/acpi/actbl1.h
+ */
+
+struct acpi_madt_local_apic {
+	struct acpi_subtable_header header;
+	uint8_t processor_id;
+	uint8_t apic_id;
+	uint32_t flags;
+};
+
+struct acpi_madt_io_apic {
+	struct acpi_subtable_header header;
+	uint8_t id;
+	uint8_t reserved;
+	uint32_t address;
+	uint32_t global_irq_base;
+};
+
+struct acpi_madt_interrupt_override {
+	struct acpi_subtable_header header;
+	uint8_t bus_source;
+	uint8_t irq_source;
+	uint32_t global_irq;
+	uint16_t flags;
+};
+
+struct acpi_madt_nmi_source {
+	struct acpi_subtable_header header;
+	uint16_t flags;
+	uint32_t global_irq;
+};
+
+struct acpi_madt_local_apic_nmi {
+	struct acpi_subtable_header header;
+	uint8_t processor_id;
+	uint16_t flags;
+	uint8_t lint;
+};
+
+struct acpi_madt_local_apic_override {
+	struct acpi_subtable_header header;
+	uint16_t reserved;
+	uint64_t address;
+};
+
+struct acpi_madt_io_sapic {
+	struct acpi_subtable_header header;
+	uint8_t id;
+	uint8_t reserved;
+	uint32_t global_irq_base;
+	uint64_t address;
+};
+
+struct acpi_madt_local_sapic {
+	struct acpi_subtable_header header;
+	uint8_t processor_id;
+	uint8_t sapic_id;
+	uint8_t sapic_eid;
+	uint8_t reserved[3];
+	uint32_t lapic_flags;
+	uint32_t uid;
+	char uid_string[1];
+};
+
+struct acpi_madt_interrupt_source {
+	struct acpi_subtable_header header;
+	uint16_t inti_flags;
+	uint8_t type;
+	uint8_t id;
+	uint8_t eid;
+	uint8_t io_sapic_vector;
+	uint32_t global_irq;
+	uint32_t flags;
+};
+
+struct acpi_madt_local_x2apic {
+	struct acpi_subtable_header header;
+	uint16_t reserved;
+	uint32_t local_apic_id;
+	uint32_t lapic_flags;
+	uint32_t uid;
+};
+
+struct acpi_madt_local_x2apic_nmi {
+	struct acpi_subtable_header header;
+	uint16_t flags;
+	uint32_t uid;
+	uint8_t lint;
+	uint8_t reserved[3];
+};
+
+struct acpi_madt_generic_interrupt {
+	struct acpi_subtable_header header;
+	uint16_t reserved;
+	uint32_t cpu_interace_number;
+	uint32_t uid;
+	uint32_t flags;
+	uint32_t parking_version;
+	uint32_t performance_interrupt;
+	uint64_t parked_address;
+	uint64_t base_address;
+	uint64_t gicv_base_address;
+	uint64_t gich_base_address;
+	uint32_t vgic_interrupt;
+	uint64_t gicr_base_address;
+	uint64_t arm_mpidr;
+	uint8_t efficiency_class;
+	uint8_t reserved2[3];
+};
+
+struct acpi_madt_generic_distributor {
+	struct acpi_subtable_header header;
+	uint16_t reserved;
+	uint32_t gic_id;
+	uint64_t base_address;
+	uint32_t global_irq_base;
+	uint8_t version;
+	uint8_t reserved2[3];
+};
+
+struct acpi_madt_generic_msi_frame {
+	struct acpi_subtable_header header;
+	uint16_t reserved;
+	uint32_t msi_frame_id;
+	uint64_t base_address;
+	uint32_t flags;
+	uint16_t spi_count;
+	uint16_t spi_base;
+};
+
+struct acpi_madt_generic_redistributor {
+	struct acpi_subtable_header header;
+	uint32_t reserved;
+	uint64_t base_address;
+	uint32_t length;
+};
+
+struct acpi_madt_generic_translator {
+	struct acpi_subtable_header header;
+	uint16_t reserved;
+	uint32_t translation_id;
+	uint64_t base_address;
+	uint32_t reserved2;
+};
+
+#endif /* ACPI_TABLES_MADT_H */
