3b7356804cd0b3bfd8cf8a7706fd10b94d18d6d6
Alexei Frolov <alexeifrolov3@gmail.com>
2017-06-02 09:29:49 -0400

    Start work on per-cpu variables

---
+++ arch/i386/include/radix/asm/percpu.h
@@ -0,0 +1,21 @@
+/*
+ * arch/i386/include/radix/asm/percpu.h
+ * Copyright (C) 2016-2017 Alexei Frolov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define __ARCH_PER_CPU_SECTION ".percpu_data"
+
+#include <radix/percpu.h>
