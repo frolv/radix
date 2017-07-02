6ac99091f3a6097953ea860953b95430786257db
Alexei Frolov <alexeifrolov3@gmail.com>
2017-07-02 00:03:30 -0400

    rconfig: process files given as arguments

--- util/rconfig/rconfig.c
+++ util/rconfig/rconfig.c
@@ -163,7 +163,8 @@ static int verify_src_dirs(const char *prog)
 
 static void usage(FILE *f, const char *prog)
 {
-	fprintf(f, "usage: %s --arch=ARCH [-d|-l] [FILE]...\n", prog);
+	fprintf(f, "usage: %s --arch=ARCH [-d|-l] [-o OUTFILE] [FILE]...\n",
+	        prog);
 	fprintf(f, "Configure a radix kernel\n");
 	fprintf(f, "\n");
 	fprintf(f, "If FILE is provided, only process given rconfig files.\n");
@@ -178,6 +179,8 @@ static void usage(FILE *f, const char *prog)
 	fprintf(f, "        print this help text and exit\n");
 	fprintf(f, "    -l, --lint\n");
 	fprintf(f, "        verify rconfig file syntax and structure\n");
+	fprintf(f, "    -o, --output=OUTFILE\n");
+	fprintf(f, "        write output to OUTFILE\n");
 }
 
 static struct option long_opts[] = {
@@ -185,6 +188,7 @@ static struct option long_opts[] = {
 	{ "default",    no_argument,       NULL, 'd' },
 	{ "help",       no_argument,       NULL, 'h' },
 	{ "lint",       no_argument,       NULL, 'l' },
+	{ "output",     required_argument, NULL, 'o' },
 	{ 0, 0, 0, 0 }
 };
 
@@ -192,9 +196,12 @@ int main(int argc, char **argv)
 {
 	char arch_dir[ARCHDIR_BUFSIZE];
 	int c, err, def;
+	struct stat sb;
+	char outfile[256] = "config/config";
 
 	def = is_linting = exit_status = 0;
-	while ((c = getopt_long(argc, argv, "a:dhl", long_opts, NULL)) != EOF) {
+	while ((c = getopt_long(argc, argv, "a:dhlo:", long_opts, NULL))
+	       != EOF) {
 		switch (c) {
 		case 'a':
 			snprintf(arch_dir, ARCHDIR_BUFSIZE, "arch/%s", optarg);
@@ -208,6 +215,9 @@ int main(int argc, char **argv)
 			return 0;
 		case 'l':
 			is_linting = 1;
+			break;
+		case 'o':
+			snprintf(outfile, 256, optarg);
 			break;
 		default:
 			usage(stderr, argv[0]);
@@ -231,6 +241,18 @@ int main(int argc, char **argv)
 		return 1;
 
 	if (optind != argc) {
+		for (; optind < argc; ++optind) {
+			if (stat(argv[optind], &sb) != 0) {
+				perror(argv[optind]);
+				exit_status = 1;
+			} else if (!S_ISREG(sb.st_mode)) {
+				fprintf(stderr, "%s: not a regular file\n",
+				        argv[optind]);
+				exit_status = 1;
+			} else {
+				rconfig_parse_file(argv[optind], def);
+			}
+		}
 	} else {
 		rconfig_recursive(def);
 	}
