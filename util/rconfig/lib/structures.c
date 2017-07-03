6e3709a4e80983e70821649f931fdddae7ecd082
Alexei Frolov <alexeifrolov3@gmail.com>
2017-07-03 19:06:18 -0400

    Fix config identifier validity check
    Config identifiers must contain only uppercase letters, underscores and
    digits. The current test for this was broken as it didn't allow digits.
    The lex scanner already ensures that an identifier matches [a-zA-Z0-9_],
    so to ensure the identifier is valid, the only check required is that no
    character is lowercase.

--- util/rconfig/lib/structures.c
+++ util/rconfig/lib/structures.c
@@ -150,7 +150,7 @@ int verify_config(struct rconfig_file *file, struct rconfig_config *conf)
 	char *s;
 
 	status = 0;
-	for (s = conf->identifier; *s && (isupper(*s) || *s == '_'); ++s)
+	for (s = conf->identifier; *s && !islower(*s); ++s)
 		;
 	if (*s) {
 		if (is_linting)
