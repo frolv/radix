cf42bf7d54f09c21d76f25ad6f1c0c1cc43a39d0
Alexei Frolov <alexeifrolov3@gmail.com>
2017-09-01 21:01:56 -0400

    Remove tick collection from the ACPI PM timer
    Updated the functioning of the ACPI PM by removing the total tick
    tracker, as this will now be standardized across all timers through
    the timer subsystem. Alongside this, removed the periodic PIT IRQ
    which updated the ACPI PM timer. This behaviour will also be
    standardized across all system timers through the event system.

--- arch/i386/timers/acpi_pm.c
+++ arch/i386/timers/acpi_pm.c
@@ -22,8 +22,6 @@
 #include <radix/io.h>
 #include <radix/spinlock.h>
 #include <radix/timer.h>
-/* TODO: remove this include */
-#include <radix/cpu.h>
 
 #define ACPI_PM_FREQUENCY       3579545
 #define ACPI_PM_MULT            2288559
@@ -35,77 +33,30 @@
  * approximately 279ns, making it a very reasonable choice for a system timer
  * source.
  *
- * However, there is one problem with the ACPI PM timer: its counter is either
- * 32 bits or 24 bits long, causing it to overflow frequently. A 24-bit counter
- * will overflow every ~4.6s, and the 32-bit counter will overflow every ~20m.
- * Due to this, it is possible for tick information to be lost if the counter
- * overflows twice without a read occurring in between. While it is unlikely
- * that the system runs for 4.6 seconds without reading the time, the possiblity
- * of tick loss must be prevented. This is done by setting up an event to
- * periodically read the counter.
- *
- * The counter limitation lowers the rating of the ACPI PM timer, as well as
- * the fact that counter reads require port access. Despite this, it is still
- * a solid choice as a system timer source.
+ * The fact that ACPI PM counter reads require port access lower its rating.
+ * Despite this, it is still a solid choice as a system timer source.
  */
 
 static uint32_t acpi_pm_port;
-static uint32_t acpi_pm_prev_ticks = 0;
-static uint64_t acpi_pm_total_ticks = 0;
-
-static spinlock_t acpi_pm_lock = SPINLOCK_INIT;
 
 static struct timer acpi_pm;
 
 /*
  * acpi_pm_read:
- * Read the curent value of the ACPI PM counter and update total ticks.
+ * Read the curent value of the ACPI PM counter.
  */
 static uint64_t acpi_pm_read(void)
 {
-	uint32_t ticks;
-	int diff;
-
-	spin_lock(&acpi_pm_lock);
-	ticks = inl(acpi_pm_port);
-	diff = ticks - acpi_pm_prev_ticks;
-
-	/* If diff < 0, i.e. prev_ticks > ticks, the counter has overflowed. */
-	if (diff < 0)
-		acpi_pm_total_ticks += (acpi_pm.max_ticks - acpi_pm_prev_ticks)
-		                       + ticks;
-	else
-		acpi_pm_total_ticks += diff;
-
-	acpi_pm_prev_ticks = ticks;
-	spin_unlock(&acpi_pm_lock);
-
-	return acpi_pm_total_ticks;
+	return inl(acpi_pm_port);
 }
 
-static void acpi_pm_update(void)
-{
-	acpi_pm_read();
-}
-
-/*
- * acpi_pm_enable:
- * Nothing needs to be done to enable the ACPI PM timer counter as it is
- * always running. However, in order to ensure that a two counter overflows
- * do not occur without a read in between (causing ticks to be lost), an
- * event is set up to read the current counter value four times every second.
- */
 static int acpi_pm_enable(void)
 {
-	acpi_pm_prev_ticks = inl(acpi_pm_port);
-
-	/* TODO: replace this when we get a proper event framework */
-	return pit_setup_periodic_irq(4, acpi_pm_update);
+	return 0;
 }
 
 static int acpi_pm_disable(void)
 {
-	pit_stop_periodic_irq();
 	return 0;
 }
 
