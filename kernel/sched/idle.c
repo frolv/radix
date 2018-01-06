1f02bb0c633152fa1e3f066c9fd4c368067cecbe
Alexei Frolov <alexeifrolov3@gmail.com>
2018-01-06 18:18:51 -0500

    Add CPU idle task
    Created an idle task to be run whenever a processor does not have
    anything else to do. For now, the idle task simply halts in an
    infinite loop. However, in the future, it will be updated to try
    to steal tasks from other processors.

---
+++ kernel/sched/idle.c
@@ -0,0 +1,57 @@
+/*
+ * kernel/sched/idle.c
+ * Copyright (C) 2017 Alexei Frolov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <radix/error.h>
+#include <radix/kernel.h>
+#include <radix/kthread.h>
+#include <radix/klog.h>
+#include <radix/percpu.h>
+#include <radix/smp.h>
+#include <radix/task.h>
+
+DEFINE_PER_CPU(struct task *, idle_task);
+
+static void idle_func(void *p)
+{
+	while (1)
+		HALT();
+
+	(void)p;
+}
+
+int idle_task_init(void)
+{
+	struct task *idle;
+	int cpu;
+
+	cpu = processor_id();
+
+	idle = kthread_create(idle_func, NULL, 0, "idle_%u", cpu);
+	if (IS_ERR(idle)) {
+		klog(KLOG_ERROR, "failed to initialize idle task for cpu %u",
+		     cpu);
+		return 1;
+	}
+
+	idle->cpu_restrict = CPUMASK_SELF;
+	idle->remaining_time = 0;
+
+	this_cpu_write(idle_task, idle);
+
+	return 0;
+}
